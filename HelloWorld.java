7f060806afcb44a933fbcaf48f39741bf6357185
bp <280077681@qq.com>
2020-04-29 19:18:15 +0800

    hello2

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public clss HelloWorld{
+   public static void main(String[] args) {
+	    System.out.println("hello5");
+		System.out.println("hello1");
+	   System.out.println("hello2"); 
+	   System.out.println("hello3"); 
+	   System.out.println("hello4"); 
+	  
+   }
+}
